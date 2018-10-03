10a4a1ecdc2e516f143d550e2de4ce0aacf70910
open24ml <43588834+open24ml@users.noreply.github.com>
2018-10-03 11:16:43 -0300

    Add files via upload

--- application/controllers/tickets.php
+++ application/controllers/tickets.php
@@ -10,7 +10,7 @@ class Tickets extends CI_Controller {
 	
 	public function index()
 	{
-		//$this->load->view('');
+		$this->load->view('principal');
 	}
 	
 	public function Agregar(){
@@ -21,7 +21,7 @@ class Tickets extends CI_Controller {
 		$this->form_validation->set_rules('txtDescripcion', 'txtDescripcion', 'required|trim');
 		$this->form_validation->set_rules('txtSector', 'txtSector', 'required|trim');
 		$this->form_validation->set_rules('txtPrioridad', 'txtPrioridad', 'required|trim');
-		//$this->form_validation->set_rules('fecha_limite', 'txtParaCuando', 'required|trim');
+		$this->form_validation->set_rules('fecha_limite', 'txtParaCuando', 'required|trim');
 		
 		$this->form_validation->run();
 		
@@ -32,7 +32,7 @@ class Tickets extends CI_Controller {
 		$datos["descripcion"] =set_value("txtDescripcion");
 		$datos["sector"] =set_value("txtSector");
 		$datos["prioridad"] =set_value("txtPrioridad");
-		//$datos["fecha_limite"] =set_value("fecha_limite");
+		$datos["fecha_limite"] =set_value("fecha_limite");
 		if ($this->session->userdata("email"))
 			$datos["creador"] =$this->session->userdata("usuario_id");
 		else
@@ -40,6 +40,6 @@ class Tickets extends CI_Controller {
 			
 		$this->ticket_model->alta($datos);
 		
-		redirect('usuarios/index/OK');	
+		redirect('tickets');	
 	}
 }
